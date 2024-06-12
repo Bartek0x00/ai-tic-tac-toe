c0b0ecf4b5fa620d982ee87466cd22e1a16bffad
Bartek0x00 <rosbar008@gmail.com>
2024-06-12 18:56:44 +0200

    fixed

--- src/AI/Solve.c
+++ src/AI/Solve.c
@@ -3,15 +3,23 @@
 #include "Board.h"
 #include "Color.h"
 
-#define TO_SIGNED(x) (((x) == 1) ? -1 : 1)
+#define TO_SIGNED(x) (((x) == 1) ? -1 : 1) 
+
+static int isDraw(Board board)
+{
+	for (int i = 0; i < 9; i++) 
+		if (!Board_Get(board, i))
+			return 0;
+	return 1;
+}
 
 static int negamax(Board board, Color color)
 {
 	int eval = Board_GetWinner(board);
 	if (eval != 0)
 		return TO_SIGNED(eval) * TO_SIGNED(color);
-	
-	int bestScore = INT_MIN;
+	if (isDraw(board)) return 0;
+	int bestScore = -2;
 	for (int i = 0; i < 9; i++) {
 		if (!Board_Get(board, i)) {
 			int score = -negamax(
@@ -28,7 +36,7 @@ static int negamax(Board board, Color color)
 
 Board solve(Board board, Color color)
 {
-	int bestScore = INT_MIN;
+	int bestScore = -2;
 	int bestMove = -1;
 
 	for (int i = 0; i < 9; i++) {
--- src/main/main.c
+++ src/main/main.c
@@ -23,45 +23,32 @@ int main(void)
 
 	while (remainingMoves) {	
 		drawDisplay(display, board);
-		switch(Board_GetWinner(board)) {
-		case NONE:
-			if (remainingMoves) {
-				board = solve(board, (playerColor ^ 0b11));
-				if (--remainingMoves)
-					goto User;
-			}
-			printf("%sIt's a TIE%s\n", T_GREEN, T_CLR);
-			goto Exit;
-		case RED:
-			if (playerColor == RED)
-				printf("%sYou've won%s\n", T_RED, T_CLR);
-			else
-				printf("%sYou've lost%s\n", T_BLUE, T_CLR);
-			goto Exit;
-		case BLUE:
-			if (playerColor == BLUE)
-				printf("%sYou've won%s\n", T_BLUE, T_CLR);
-			else
-				printf("%sYou've lost%s\n", T_RED, T_CLR);
-			goto Exit;
+		Color winner = Board_GetWinner(board);
+		if (winner) {
+			printf("You've lost");
+			break;
+		} else if (remainingMoves--) {
+			board = solve(
+				board, 
+				(playerColor ^ 0b11)
+			);
+			if (remainingMoves) goto User;
+		} else {
+			printf("%sIt's a tie%s", T_GREEN, T_CLR);
+			break;
 		}
 
 		User:
 		drawDisplay(display, board);
 		while(!getEvent(&input));
-		if (input == 'q') {
-			goto Exit;
-		} else if (input >= '0' && input <= '8') {
-			input -= '0';
-			if(!Board_Get(board, input)) {
-				board = Board_Set(board, input, playerColor);
-				remainingMoves--;
-				printf("user: %u\n", input);
-			}
+
+		if (input == 'q') break;
+		if (!Board_Get(board, input)) {
+			board = Board_Set(board, (input - '0'), playerColor);
+			remainingMoves--;
 		}		
 	}
 
-Exit:
 	deleteDisplay(display);
 	return 0;
 }
